65f6605a1373323bfb296959f3a154e95a00a962
Vitorakavitor <49196415+Vitorakavitor@users.noreply.github.com>
2023-09-28 11:24:55 -0300

    Add files via upload

---
+++ src/Forum.js
@@ -0,0 +1,13 @@
+// Forum.js
+import React from 'react';
+
+function Forum() {
+  return (
+    <div>
+      <h1>Página do Fórum</h1>
+      {/* Conteúdo da página do fórum */}
+    </div>
+  );
+}
+
+export default Forum;
